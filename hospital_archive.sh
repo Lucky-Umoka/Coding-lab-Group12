3ec4825cdd19cfca6c8636b34c42108c122c5c07
hotay11 <j.hotay@alustudent.com>
2026-06-02 12:51:13 -0700

    Archiving the files

---
+++ hospital_archive.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "Archiving logs..."
+
+timestamp=$(date +"%Y%m%d_%H%M%S")
+
+mv active_logs/heart_rate_log.log archived_logs/heart_rate_log_$timestamp.log
+mv active_logs/temperature_log.log archived_logs/temperature_log_$timestamp.log
+mv active_logs/water_usage_log.log archived_logs/water_usage_log_$timestamp.log
+
+touch active_logs/heart_rate_log.log
+touch active_logs/temperature_log.log
+touch active_logs/water_usage_log.log
+
+echo "Archive complete."
