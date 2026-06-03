a4da8f3b31dd37dfd0747a87c283e419b5abebf1
hotay11 <j.hotay@alustudent.com>
2026-06-03 01:12:53 -0700

    hospital_archive.sh rectifications

--- hospital_archive.sh
+++ hospital_archive.sh
@@ -2,11 +2,11 @@
 
 echo "Archiving logs..."
 
-timestamp=$(date +"%Y%m%d_%H%M%S")
+timestamp=$(date +"%Y%m%d_%H%M")
 
-mv active_logs/heart_rate_log.log archived_logs/heart_rate_log_$timestamp.log
-mv active_logs/temperature_log.log archived_logs/temperature_log_$timestamp.log
-mv active_logs/water_usage_log.log archived_logs/water_usage_log_$timestamp.log
+mv active_logs/heart_rate_log.log archived_logs/heart_rate_$timestamp.log
+mv active_logs/temperature_log.log archived_logs/temperature_$timestamp.log
+mv active_logs/water_usage_log.log archived_logs/water_usage_$timestamp.log
 
 touch active_logs/heart_rate_log.log
 touch active_logs/temperature_log.log
